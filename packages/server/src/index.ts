02b2ee60cc9e67c3071007bd598d453a652b584e
VincentSJM <vincentsjm@qq.com>
2019-04-17 20:31:11 -0400

    feat(client): init client

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { resolve } from "path";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static(resolve(__dirname,'../../client/dist')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
