51c6ee5a32e2d7763aab70652caffe44c560c5b0
VincentSJM <vincentsjm@qq.com>
2019-04-17 21:58:34 -0400

    fix(client): unregister service worker

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './registerServiceWorker';
+// import './registerServiceWorker';
+// import {unregister} from 'register-service-worker';
+// unregister();
 
 Vue.config.productionTip = false;
 
